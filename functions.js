f3dbe538895fc26198d1a6883be7693e96b1887a
JoanneOs <jowan.awsman@yahoo.com>
2025-03-05 15:38:11 -0600

    Average array

--- functions.js
+++ functions.js
@@ -4,3 +4,10 @@ function jSumArrayAlt(numbers){let jSum=0;
     numbers.forEach(jNum => Jsum += JNum);
     returnjSum;
 }
+//taking array numbers and return teh avarage using for
+function jAverageArrayAlt(numbers){let JSum=0;
+    for (let j=0;j<numbers.length;j++){
+        jSum +=numbers[j];
+    }
+    return numbers.length>0?jSum/numbers.length:0;
+}
